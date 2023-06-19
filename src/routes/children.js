1853ffc6c26afb3d0fee612a2f3ea15879088ada
ncn <namnguyencong23@gmail.com>
2023-06-19 08:51:06 +0700

    19/06

--- src/routes/children.js
+++ src/routes/children.js
@@ -1,13 +1,13 @@
-// const express = require('express')
+const express = require('express')
 
-// const router = express.Router();
+const router = express.Router();
 
-// const childrenControllers = require('../controllers/children');
+const childrenControllers = require('../controllers/children');
 
-// router.post('/children', childrenControllers.sendData);
+router.post('/children', childrenControllers.sendData);
 
-// router.get('/children', childrenControllers.getAllChildren);
+router.get('/children', childrenControllers.getAllChildren);
 
-// router.get('/search-country', childrenControllers.getDataByCountry);
+router.get('/search-country', childrenControllers.getDataByCountry);
 
-// module.exports = router;
+module.exports = router;
