abb5ef430cea954c52bcd80a7abe55e2e27b73e1
ncn <namnguyencong23@gmail.com>
2023-06-20 16:54:17 +0700

    Exercise 20/06

--- src/exercise-javacore/Calculator.java
+++ src/exercise-javacore/Calculator.java
@@ -98,10 +98,10 @@ public class Calculator {
         System.out.println("pyramid " + a + " floor");
         for (int i = 1; i <= a; i++) {
             for (int j = 1; j <= a - i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k = 1; k <= i + i - 1; k++) {
-                System.out.print("*");
+                System.out.print(" *");
             }
             System.out.println();
         }
